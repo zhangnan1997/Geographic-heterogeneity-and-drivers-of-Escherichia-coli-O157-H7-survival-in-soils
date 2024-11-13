ef4a8728fbebc8a6de9bf318c73e2462becda0cb
zhangnan1997 <zhangnan19970@163.com>
2024-11-13 21:05:38 +0800

    Update Network_analysis.R

--- Network_analysis.R
+++ Network_analysis.R
@@ -3,32 +3,27 @@ library(igraph)
 #input data
 genus <- read.csv("inputdata.csv",header = T,row.names = 1)
 
-#filtered data
+#filtered data, it depends on the inputdata
 b=dim(genus)
 genus <- genus[which(rowSums(genus) >= 0.005), ]    
 genus1 <- genus
 genus1[genus1>0] <- 1
 genus <- genus[which(rowSums(genus1) >= round(b[2]*20/100)), ]    
-
 #spearman correlation
 genus_corr <- rcorr(t(genus), type = 'spearman')
-
 # r>=0.6
 r <- genus_corr$r
 r[abs(r) < 0.6] <- 0
-
 # p<0.05
 p <- genus_corr$P
 p <- p.adjust(p, method = 'BH')    
 p[p>=0.05] <- -1
 p[p<0.05 & p>=0] <- 1
 p[p==-1] <- 0
-
 #Data is retained according to the r and p values filtered above
 z <- r * p
 diag(z) <- 0    #Converts the value in the diagonal of the correlation matrix (which represents the autocorrelation) to 0
 head(z)[1:6,1:6]
-
 ##Acquisition network
 g <- graph.adjacency(z, weighted = TRUE, mode = 'undirected')
 g <- simplify(g)
